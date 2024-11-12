3a3e2907e9c221daf1a222afa2a6d6a5371b1c94
yeeunSim <wjdwlr03@gmail.com>
2024-11-12 21:54:06 +0900

    Refactor: 심예은.java

--- programmers/340199/심예은.java
+++ programmers/340199/심예은.java
@@ -1,44 +1,34 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] wallet, int[] bill) {
         int answer = 0;
 
         while (true) {
-            // 지갑 50 * 50, 지폐 100 * 241
             // 지갑의 크기보다 지폐의 크기가 작은지, 큰지 확인
             // 지갑과 지폐 배열의 최소값, 최댓값을 각각 구하기
-            int wallet_min = Integer.MIN_VALUE;
-            int wallet_max = Integer.MIN_VALUE;
+            int wallet_min = Arrays.stream(wallet).min().getAsInt();
+            int wallet_max = Arrays.stream(wallet).max().getAsInt();
 
-            for (int number : wallet) {
-                if (number < wallet_min) wallet_min = number; // 50
-                if (number > wallet_max) wallet_max = number; // 50
-            }
+            int bill_min = Arrays.stream(bill).min().getAsInt();
+            int bill_max = Arrays.stream(bill).max().getAsInt();
 
-            int bill_min = Integer.MAX_VALUE;
-            int bill_max = Integer.MIN_VALUE;
-
-            for (int number : bill) {
-                if (number < bill_min) bill_min = number; // 100, 100, 60, 50, 30
-                if (number > bill_max) bill_max = number; // 241, 120, 100, 60, 50
-            }
-
-            // 100 > 50 || 241 > 50, 100 > 50 || 120 > 50, 60 > 50 || 100 > 50, 50 > 50 || 60 > 50, 30 > 50 || 50 > 50
             if(bill_min > wallet_min || bill_max > wallet_max) {
                 // 지폐 반으로 접기
-                if (bill[0] > bill[1]) { // 100 > 241, 120 > 100, 60 > 100, 50 > 60
-                    bill[0] /= 2; // 60
+                if (bill[0] > bill[1]) {
+                    bill[0] /= 2;
                 } else {
-                    bill[1] /= 2; // 120, 50, 30
+                    bill[1] /= 2;
                 }
 
                 // 90도 돌리기
                 int temp = 0;
                 temp = bill[0];
-                bill[0] = bill[1]; // 120, 100, 50, 30
-                bill[1] = temp; // 100, 60, 60, 50
+                bill[0] = bill[1];
+                bill[1] = temp;
 
                 // 접는 횟수 증가
-                answer++; // 1, 2, 3, 4
+                answer++;
             } else {
                 break;
             }
