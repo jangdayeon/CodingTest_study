3f3b2ab9b4e1e0c22116548520c31040770d6a19
yeeunSim <wjdwlr03@gmail.com>
2025-01-23 11:35:21 +0900

    Chore: rebase and merge 오류로 인한 임시 커밋 추가

--- programmers/10주차/12924/심예은.java
+++ programmers/10주차/12924/심예은.java
@@ -1,6 +1,5 @@
 public class Solution {
-    public int solution(int n) {
-        int count = 0;
+    public int solution(int n) // cnt 역할
 
         // 연속된 자연수의 시작 숫자를 i로 설정
         for (int i = 1; i <= n; i++) {
@@ -11,13 +10,13 @@ public class Solution {
                 sum += j;
 
                 if (sum == n) {
-                    count++; // n에 도달하면 경우의 수 증가
+                    answer++; // n에 도달하면 경우의 수 증가
                     break;
                 } else if (sum > n) {
                     break; // 합이 n을 초과하면 더 이상 확인하지 않음
                 }
             }
         }
-        return count;
+        return answer;
     }
 }
