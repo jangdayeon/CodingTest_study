959ac1d9315138848ee59515efb8cfebaa6b9842
yeeunSim <wjdwlr03@gmail.com>
2025-02-02 16:45:59 +0900

    Add: yeeun-12953

--- programmers/12주차/12953/심예은.java
+++ programmers/12주차/12953/심예은.java
@@ -1,18 +1,18 @@
-import java.util.LinkedList;
-
 public class Solution {
-    public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
-        LinkedList<String> word = new LinkedList<>();
+    public int solution(int[] arr) {
+        int answer = arr[0];
 
-        for (int i = 0; i < words.length; i++) {
-            if (i > 0 && (word.contains(words[i]) || words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0))) {
-                answer[0] = (i % n) + 1;
-                answer[1] = (i / n) + 1;
-                return answer;
-            }
-            word.add(words[i]);
+        for (int i = 1; i < arr.length; i++) {
+            answer = lcm(answer, arr[i]);
         }
         return answer;
+    }
+
+    public static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
     }
 }
