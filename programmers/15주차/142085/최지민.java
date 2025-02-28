68e63292269815d748a691ca293e2310399fe9f0
choijimin <jiminchoi1221@gmail.com>
2025-02-28 20:15:19 +0900

    Add: jimin-142085

---
+++ programmers/15주차/142085/최지민.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i = 0; i < enemy.length; i++) {
+            n -= enemy[i];
+            pq.add(enemy[i]);
+            
+            if(n < 0) {
+                if(k > 0) {
+                    n += pq.poll();
+                    k -= 1;
+                }
+                else return i;
+            }
+        }
+        
+        return enemy.length;
+    }
+}
