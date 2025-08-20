b5f73ebcc05997de3cdd1c0843b7cd5a48db0be3
choi-jimin <jiminchoi1221@gmail.com>
2025-08-20 19:54:49 +0900

    Add: jimin-12904

---
+++ programmers/37주차/12907/최지민.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            answer = Math.max(answer, check(s, i, i + 1)); // 짝
+            answer = Math.max(answer, check(s, i, i) - 1); // 홀
+        }
+
+        return answer;
+    }
+    
+    private int check(String s, int l, int r) {
+        int cnt = 0;
+        
+        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+            cnt++;
+        }
+        
+        return cnt * 2;
+    }
+}
